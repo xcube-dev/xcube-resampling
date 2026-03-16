0ab06ed0bc2d9f26d13d603d30c22aeda7fb4035
konstntokas <konstantin.ntokas@brockmann-consult.de>
2026-03-16 13:54:00 +0100

    remove zarr reading

--- tests/gridmapping/test_dataset.py
+++ tests/gridmapping/test_dataset.py
@@ -80,30 +80,30 @@ class DatasetGridMappingTest(unittest.TestCase):
         self.assertTrue(result.is_regular)
         self.assertEqual(result.crs.to_string(), "EPSG:4326")
 
-    def test_from_real_olci(self):
-        olci_l2_path = os.path.join(
-            os.path.dirname(__file__),
-            "..",
-            "..",
-            "examples",
-            "inputdata",
-            "S3-OLCI-L2A.zarr.zip",
-        )
-
-        dataset = xr.open_zarr(olci_l2_path, consolidated=False)
-        gm = GridMapping.from_dataset(dataset)
-        self.assertEqual((1189, 1890), gm.size)
-        self.assertEqual((512, 512), gm.tile_size)
-        self.assertEqual(GEO_CRS, gm.crs)
-        self.assertEqual((0.00447132, 0.00255235), gm.xy_res)
-        self.assertEqual(False, gm.is_regular)
-        self.assertEqual(False, gm.is_lon_360)
-        self.assertEqual(False, gm.is_j_axis_up)
-        self.assertEqual((2, 1890, 1189), gm.xy_coords.shape)
-        self.assertEqual(("coord", "y", "x"), gm.xy_coords.dims)
-
-        gm = gm.to_regular()
-        self.assertEqual((1636, 2132), gm.size)
+    # def test_from_real_olci(self):
+    #     olci_l2_path = os.path.join(
+    #         os.path.dirname(__file__),
+    #         "..",
+    #         "..",
+    #         "examples",
+    #         "inputdata",
+    #         "S3-OLCI-L2A.zarr",
+    #     )
+    #
+    #     dataset = xr.open_zarr(olci_l2_path, consolidated=False)
+    #     gm = GridMapping.from_dataset(dataset)
+    #     self.assertEqual((1189, 1890), gm.size)
+    #     self.assertEqual((512, 512), gm.tile_size)
+    #     self.assertEqual(GEO_CRS, gm.crs)
+    #     self.assertEqual((0.00447132, 0.00255235), gm.xy_res)
+    #     self.assertEqual(False, gm.is_regular)
+    #     self.assertEqual(False, gm.is_lon_360)
+    #     self.assertEqual(False, gm.is_j_axis_up)
+    #     self.assertEqual((2, 1890, 1189), gm.xy_coords.shape)
+    #     self.assertEqual(("coord", "y", "x"), gm.xy_coords.dims)
+    #
+    #     gm = gm.to_regular()
+    #     self.assertEqual((1636, 2132), gm.size)
 
     def test_from_sentinel_2(self):
         dataset = create_s2plus_dataset()
